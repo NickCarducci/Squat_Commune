58bfcb7d719f18482bbe552b7e58505917d2d76d
Nick <nmcarducci@gmail.com>
2021-03-21 18:49:37 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Squat Commune</h1>
+      <h2>not a licensed douche bag</h2>
+    </div>
+  );
+}
