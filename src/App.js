34f7530897c366f48fd9f096b0e524d8d0468d42
Nick Carducci <nmcarducci@gmail.com>
2021-03-21 19:08:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,43 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Squat Commune</h1>
-      <h2>not a licensed douche bag</h2>
-      <br />
-      <br />
-      protect yourself from us by joining!
+      <div style={{ position: "relative", maxWidth: "600px" }}>
+        <div style={{ position: "relative" }}>
+          <img
+            style={{
+              height: "auto",
+              width: "100%"
+            }}
+            alt="zoltan tasi on unsplash: sheep and lamb in meadow"
+            src="https://www.dl.dropboxusercontent.com/s/a6r9gt5iqq9ay25/zoltan-tasi-MP3VNHhItxY-unsplash.jpg?dl=0"
+          />{" "}
+        </div>
+        <h1>Squat Commune</h1>
+        <h2>not a licensed douche bag</h2>
+        <br />
+        <br />
+        protect yourself from us by joining!
+        <br />
+        <br />
+        <a href="www1.nyc.gov/assets/nypd/downloads/pdf/crime_statistics/cs-en-us-city.pdf">
+          what (3/21/21, NYPD report)
+        </a>
+        <br />
+        <div
+          style={{
+            border: "1px dotted"
+          }}
+        >
+          <a href="https://foiegras.life">Dom Carter</a>&nbsp;caller said 150%
+          up for all races, here updated 3/4/21 all time-period changes are
+          negative, the race-to-race&nbsp;
+          <a href="https://fbi.gov/news/pressrel/press-releases/overview-of-preliminary-uniform-crime-report-january-june-2020">
+            homocides
+          </a>
+          &nbsp;are still double-or-quadruple ....cross-race are you just
+          reporting racial crimes more now?
+        </div>
+      </div>
     </div>
   );
 }
