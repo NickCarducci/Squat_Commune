fd4f064b78b0443e28e8f32b5a971ce02498daa7
Nick Carducci <nmcarducci@gmail.com>
2021-03-24 16:35:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div className="App">
       <div style={{ position: "relative", maxWidth: "600px" }}>
-        YOU NOT STUPID THEN YOU'RE GUILTY (bang up job!)
+        YOU NOT STUPID THEN YOU'RE GUILTY (bang up job!). there is no collateral
+        on personal loans
         <br />
         <br />
         Rudy: "Trump will be president when he puts his social platform up"
