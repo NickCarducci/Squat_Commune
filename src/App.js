e5b8a95b6b84f284098e0b5966f4f7a935a4eb4f
Nick Carducci <nmcarducci@gmail.com>
2021-03-24 14:50:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,9 +14,10 @@ export default function App() {
         years age is obviously not a factor or is assumed that pollsters speak
         truths only when possible. Government and people are being conflated,
         people are being conflated with individuals (even if thumbprint was made
-        for one-nation alone, you cannot void copyright; it is encompassing but
-        my goal is to keep it fit and not sell-out to someone who would not
-        operate for government or profit)
+        for one-nation alone, you cannot void copyright; it is encompassing like
+        a monopoly, but it is not an input-cost or network of spectacular
+        design, but my goal is to keep it fit and not sell-out to someone who
+        would not operate for government or profit)
         <br />
         <br />
         Vote bus must be without candidate-ad; 2-2-1 at least assumes all 5
