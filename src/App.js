5b953138481c92045e20f3c11d085989f2d6d7f7
Nick Carducci <nmcarducci@gmail.com>
2021-03-24 12:20:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       <div style={{ position: "relative", maxWidth: "600px" }}>
         'there is no "negative impact to the economy [trade-volume, trade-volume
-        per capita, or ease-of-life] by banning fractional-reserves]" - Jerome
+        per capita, or ease-of-life] by banning fractional-reserves" - Jerome
         Powell, Bilenial session March 24th, 2021
         <br />
         [In fact, along all metrics in the array does 1:1
