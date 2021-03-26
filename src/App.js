015053b40c44e1701194f22052cde339bbf6e86d
Nick Carducci <nmcarducci@gmail.com>
2021-03-26 18:50:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ export default class App extends React.Component {
       <div className="App">
         <div style={{ position: "relative", maxWidth: "600px" }}>
           There is no evidence asymptomatic have virus, where vaccine would help
-          in stopping the transmission. Only if they are symptomatic is vaccine
-          useful (2 weeks after). in the case of symptomatic, you can exclude
-          from travel/business
+          in stopping the transmission (on the skin). Only if they are
+          symptomatic (thru the skin) is vaccine useful (2 weeks after). in the
+          case of symptomatic, you can exclude from travel/business
           <h3>You're an idiot, a retard or guilty</h3>
           beneficiaries get residual of what you take for only an increase in
           price instead of allowing demand to negotiate
