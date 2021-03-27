5940fad8f23067b21ef6c095ed595127a0fbcf43
Nick Carducci <nmcarducci@gmail.com>
2021-03-27 11:47:26 -0400

    Update Fraud6.js

--- src/Fraud6.js
+++ src/Fraud6.js
@@ -15,14 +15,14 @@ class Fraud6 extends React.Component {
           fontSize: "16px",
           width: "100%",
           transition: ".3s ease-out",
-          height: "950px"
+          height: "450px"
         }}
       >
         <div
           style={{
             position: "absolute",
             width: "min-content",
-            height: "950px",
+            height: "450px",
             left: "0px"
           }}
         >
@@ -32,7 +32,7 @@ class Fraud6 extends React.Component {
               height: "900px"
             }}
             title="(( (v2- (GDP/(m2-currencyComponentOfM1)) )*currencyComponentOfM1 - NM - ND) / P) / currencyComponentOfM1"
-            src={`https://fred.stlouisfed.org/graph/graph-landing.php?g=Co5w&width=600&height=800`}
+            src={`https://fred.stlouisfed.org/graph/graph-landing.php?g=Co5w&width=600&height=400`}
             scrolling="no"
             className="embed-container1"
           />
@@ -41,4 +41,4 @@ class Fraud6 extends React.Component {
     );
   }
 }
-export default Fraud6
+export default Fraud6;
