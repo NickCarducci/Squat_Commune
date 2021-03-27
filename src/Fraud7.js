d4472dd6c6a7520deb535c1b90ec8ccf565eb223
Nick Carducci <nmcarducci@gmail.com>
2021-03-27 11:56:22 -0400

    Create Fraud7.js

---
+++ src/Fraud7.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class Fraud7 extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div
+        style={{
+          overflowX: "auto",
+          overflowY: "hidden",
+          display: "flex",
+          position: "relative",
+          left: "0px",
+          flexDirection: "column",
+          fontSize: "16px",
+          width: "100%",
+          transition: ".3s ease-out",
+          height: "450px"
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            width: "min-content",
+            height: "450px",
+            left: "0px"
+          }}
+        >
+          <iframe
+            style={{
+              width: "600px",
+              height: "900px"
+            }}
+            title="(( (v2- (GDP/(m2-currencyComponentOfM1)) )*currencyComponentOfM1 - NM - ND) / P) / currencyComponentOfM1"
+            src={`https://fred.stlouisfed.org/graph/graph-landing.php?g=Co7F&width=600&height=400`}
+            scrolling="no"
+            className="embed-container1"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+export default Fraud7;
