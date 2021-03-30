36b5db1421c548287f4f1b07d32c21e784589183
Nick Carducci <nmcarducci@gmail.com>
2021-03-30 12:09:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,7 +51,8 @@ export default class App extends React.Component {
           Get out of my house&nbsp;
           <a href="https://fred.stlouisfed.org/series/TCMDO">squatters</a>
           <h3>
-            "Please use experience[ about responsive questions], not opinion"
+            "Please use experience[ about responsive questions], not opinion[
+            about integrals]"
           </h3>
           As you tighten budget for free-rider-immutable services only useless
           price-hikes cease. Keystone
